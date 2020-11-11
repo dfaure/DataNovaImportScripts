4ea0eaeb96a8732d964ca0e23c711f84a9c6181c
David Faure <faure@kde.org>
2020-11-11 11:48:44 +0100

    Prepare for github

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # DataNovaImportScripts
 Import scripts for datanova.laposte.fr into OSM
+
+See https://wiki.openstreetmap.org/wiki/Import/FrenchPostOfficeOpeningHours
--- prepare_import.sh
+++ prepare_import.sh
@@ -1,5 +1,5 @@
 ./parse.pl ../laposte_ouvertur.csv > ../out 2> ../warnings
 ready=`grep -v ERROR ../out | wc -l`
 errors=`grep ERROR ../out | wc -l`
-echo "$ready post offices ready for import. $errors post offices with unresolved rules"
+echo "$ready post offices ready for import, $errors post offices with unresolved rules."
 echo "(see ../warnings)"
